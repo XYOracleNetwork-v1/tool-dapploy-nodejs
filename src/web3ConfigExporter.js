fd2bddba23ec44d81e9783ed38fcc997ef8e15c4
Kevin Weiler <kevinw@gmail.com>
2018-08-09 14:55:01 -0700

    Remove examples folder as they get moved to their own respective repos. Update config, allow no portis if not specified in config

--- src/web3ConfigExporter.js
+++ src/web3ConfigExporter.js
@@ -2,14 +2,24 @@ const shell = require('shelljs');
 const templateFile = './src/web3template.js'
 const tempFile = './src/temp999'
 const portisConfigString = ({portisApiKey, network, appName, logoUrl}) => 
-`   return new Web3(
-        new PortisProvider({
-        apiKey: '${portisApiKey}',
-        network: '${network}',
-        appName: '${appName}',
-        appLogoUrl: '${logoUrl}',
-        }),
-    )`
+{
+    console.log(portisApiKey, network, appName, logoUrl)
+    if (!portisApiKey || !appName) {
+        // default to localhost if no portis included
+        return `return new Web3('http://localhost:8545')`
+    } else {
+        return `   return new Web3(
+            new PortisProvider({
+            apiKey: '${portisApiKey}',
+            network: '${network}',
+            appName: '${appName}',
+            appLogoUrl: '${logoUrl}',
+            }),
+        )`
+    }
+    
+}
+
 const contractDeclarationString = (contracts) => {
     let returnString = ''
     for (const contract of contracts) {
@@ -34,10 +44,10 @@ let exportConfig = (program, contracts) => {
 
     if (program.addPortis){
         shell.sed('-i', 'PORTIS_DECLARATION', `import { PortisProvider } from 'portis'`, tempFile);
-        shell.sed('-i', 'PORTIS_PROVIDER', portisConfigString(program), tempFile);
     } else {
-
+        shell.sed('-i', 'PORTIS_DECLARATION', '', tempFile);
     }
+    shell.sed('-i', 'PORTIS_PROVIDER', portisConfigString(program), tempFile);
     shell.sed('-i', 'CONTRACT_DECLARATIONS', contractDeclarationString(contracts), tempFile);
     shell.sed('-i', 'CONTRACT_INSTANTIATION', contractInstantiationString(program.contractOutput, contracts), tempFile);
 
