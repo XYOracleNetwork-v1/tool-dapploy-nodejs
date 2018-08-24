21839f4187a6b64264a3c97321776dd801498dbe
Kevin Weiler <kevinw@gmail.com>
2018-08-24 16:43:05 -0700

    Add dapper to dapploy.  Add relative paths. Allow dapploy + dapper option

---
+++ src/runDapper.js
@@ -0,0 +1,17 @@
+const { execPromise } = require('./execWrapper')
+
+/* 
+    Runs the included dapper npm so that you can view the deployed smart contracts
+*/
+const runDapper = (program) => {
+    console.log(` $ Running Dapper`);
+
+    let command = `yarn start`
+    console.log(` $ Starting dapper with command: ${command}`);
+
+            
+    let cwd = `${__dirname}/../node_modules/tool-dapper-react/`
+    return execPromise(command, { cwd })
+}
+
+module.exports = { runDapper }
